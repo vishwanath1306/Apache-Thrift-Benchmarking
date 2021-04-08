3f3f2853adb7b8cd8334a80d19ec7823d41a70ce
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2021-04-08 12:40:53 -0400

    Added: REST Proxy for MLInfer Service
    * REST Client for MLInfer TThreaded Service.
    * Fixed: Boost log not working for MLInfer TTD.
    * Modified: Makefile for MLInfer TTD.

--- MLInfer/tthreaded/compute_client.cpp
+++ MLInfer/tthreaded/compute_client.cpp
@@ -11,12 +11,14 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <cstdlib>
 #include <vector>
+#include <pistache/endpoint.h>
 #define BOOST_LOG_DYN_LINK 1
 
 using namespace apache::thrift::transport;
 using namespace apache::thrift::protocol;
 using std::make_shared;
 using std::shared_ptr;
+using namespace Pistache;
 
 namespace logging = boost::log;
 namespace keywords = boost::log::keywords;
@@ -24,48 +26,68 @@ namespace keywords = boost::log::keywords;
 void init_logging(){
     logging::add_file_log(
         
-        keywords::file_name="mlinfer_ttd.log",
+        keywords::file_name="valimai_update.log",
         keywords::open_mode=std::ios_base::app,
-        keywords::target_file_name="mlinfer_ttd.log",
-        keywords::format = "[%TimeStamp%]  [%ThreadID%] %Message%"
+        keywords::target_file_name="valimai.log",
+        keywords::format = "[%TimeStamp%]  [%ThreadID%] %Message%",
+        keywords::auto_flush = true
         );
 
         logging::add_common_attributes();   
 }
 
-int main(){
+class RESTProxyHandler: public Http::Handler{
 
-    init_logging();
+    private:
+        shared_ptr<TFramedTransport> mlinfer_transport;
+        shared_ptr<ComputeClient> mlinfer_client;
     
-    srand(time(0));
+    public:
 
-    shared_ptr<TTransport> trans;
-    trans = make_shared<TSocket>("localhost", 3056);
-    trans = make_shared<TFramedTransport>(trans);
-    auto proto = make_shared<TJSONProtocol>(trans);
+        HTTP_PROTOTYPE(RESTProxyHandler)
 
-    ComputeClient client(proto);
+        RESTProxyHandler(shared_ptr<TFramedTransport> transport_param, shared_ptr<ComputeClient> client_param){
 
-    trans->open();
+            mlinfer_transport = transport_param;
+            mlinfer_client = client_param;
 
-    std::string msg;
-    int32_t row = rand() % 100;
-    int32_t column = rand() % 100;
-    
-    std::vector<int64_t> values;
-    for(int i = 0; i < 100; i++){
-        int64_t secret = rand() % 1000 + 1;
-        values.push_back(secret);
-    }
+        }
 
-    auto start = std::chrono::high_resolution_clock::now();
-    // client.computed_value(row, column);
-    // std::cout << msg << std::endl;
-    client.compute_list(msg, values);
-    std::cout<<msg<<std::endl;
-    auto elapsed = std::chrono::high_resolution_clock::now() - start;
-    long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+        void onRequest(const Http::Request& request, Http::ResponseWriter response){
 
-    BOOST_LOG_TRIVIAL(info) <<"The time to execute client (Row Column Microseconds): "<< row << " "<< column<< " "<< microseconds;
-    trans->close();
+            if(!mlinfer_transport->isOpen()){
+                mlinfer_transport->open();
+            }
+
+            std::string mono;
+            auto start = std::chrono::high_resolution_clock::now();
+            mlinfer_client->computed_value(10,10);
+            auto elapsed = std::chrono::high_resolution_clock::now() - start;
+            long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+            BOOST_LOG_TRIVIAL(info) <<"The time to execute client (Microseconds): "<< microseconds;
+            std::cout << "Finished computation" << std::endl;
+            response.send(Http::Code::Ok, "Finished Compute");
+        }
+};
+
+
+int main(int argc, char* argv[]){
+
+    shared_ptr<TTransport> socket(new TSocket("127.0.0.1", 3056));
+    shared_ptr<TFramedTransport> transport(new TFramedTransport(socket));
+    shared_ptr<TJSONProtocol> protocol(new TJSONProtocol(transport));
+    shared_ptr<ComputeClient> compute_client(new ComputeClient(protocol));
+
+    Address addr(Ipv4::any(), 9080);
+    auto opts = Http::Endpoint::options().threads(1);
+    Http::Endpoint rest_server(addr);
+    rest_server.init();
+    rest_server.setHandler(make_shared<RESTProxyHandler>(transport, compute_client));
+    std::thread rest_thread([&] () {
+        init_logging();
+        rest_server.serve();
+    });
+
+    std::cout<<"Started server at port 9080"<<std::endl;
+    rest_thread.join();
 }
