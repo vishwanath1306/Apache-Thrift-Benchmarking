1f10c7f1b1afc4a59b68c80b35575e867520d19e
Vishwanath Seshagiri <vishwa.nath@outlook.com>
2022-04-19 11:56:10 -0400

    Added loadgen for echo server
    * Added loadgen for basic hindsight exp

--- EchoServerHindsight/tthreaded/echo_client.cpp
+++ EchoServerHindsight/tthreaded/echo_client.cpp
@@ -13,24 +13,57 @@ using namespace echoservice;
 using std::make_shared;
 using std::shared_ptr;
 
-int main(int argc, char* argv[]){
+
+void run_workload_gen(int64_t seconds, int64_t reqps){
 
     shared_ptr<TTransport> trans;
     trans = make_shared<TSocket>("localhost", 3046);
     trans = make_shared<TFramedTransport>(trans);
     auto proto = make_shared<TJSONProtocol>(trans);
 
-    EchoServiceClient client(proto);
 
+    int64_t counter = 0;
+    EchoServiceClient client(proto);
     trans->open();
 
-    auto context =  new TraceContext();
-    context->req_id = 123456;
-    context->baggage = "hello world";
+    int64_t response = 0;
 
-    std::cout<<context->req_id<<" "<<context->baggage<<std::endl;
-    client.call(*context, "this is fun");
+    while(true){
+
+        if(counter == seconds){
+            break;
+        }else{
+            response = 0;
+        }
+
+        for(int64_t i = 0; i < reqps; i++){
+            auto context =  new TraceContext();
+            context->req_id = std::rand();
+            context->baggage = "";
+
+            client.call(*context, "extra information");
+        }
+    }
+
     trans->close();
+}
 
+int main(int argc, char* argv[]){
+
+    // Usage: ./client 30 10 3
+
+    int64_t seconds = atoi(argv[1]);
+    int64_t reqps = atoi(argv[2]);
+    int64_t no_of_clients = atoi(argv[3]);
+
+    std::vector<std::thread> threads;
+    for(int i = 0; i < no_of_clients; i++){
+        std::cout<<"Spawned Client: "<< i + 1<<std::endl;
+        threads.push_back(std::thread(run_workload_gen, seconds, reqps));
+    }
+
+    for(int i = 0; i <no_of_clients; i++){
+        threads[i].join();
+    }
 
 }
